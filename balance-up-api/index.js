a02fb12068d8770e9b7b3b6d83da3e5c2d9c8749
Bethany Mitch <bethany.mitch@devetry.com>
2022-10-28 16:04:47 -0600

    added a Procfile"

--- balance-up-api/index.js
+++ balance-up-api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const db = require("./queries");
 
 app.use(bodyParser.json());
@@ -17,6 +17,6 @@ app.post("/users", db.createUser);
 app.put("/users/:id", db.updateUser);
 app.delete("/users/:id", db.deleteUser);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
 });
